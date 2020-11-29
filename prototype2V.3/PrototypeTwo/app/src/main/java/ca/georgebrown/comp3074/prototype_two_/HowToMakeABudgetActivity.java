1862fe982d75dcdec31922c5ca32302f58022644
joseph-roman70 <joseph.roman@georgebrown.ca>
2020-11-28 22:59:12 -0500

    most updated project

---
+++ prototype2V.3/PrototypeTwo/app/src/main/java/ca/georgebrown/comp3074/prototype_two_/HowToMakeABudgetActivity.java
@@ -0,0 +1,99 @@
+package ca.georgebrown.comp3074.prototype_two_;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class HowToMakeABudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_make_a_budget);
+    }
+    //start sign up activity
+    private void getSignUpActivity(){
+        Intent start = new Intent(getApplicationContext(), SignUpActivity.class);
+        startActivity(start);
+    }
+    //start profile activity
+    private void getProfile(){
+        Intent start = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivity(start);
+    }
+    //start main content activity
+    private void getMainContent(){
+        Intent start = new Intent(getApplicationContext(), MainContentActivity.class);
+        startActivity(start);
+    }
+    //start welcome page activity
+    private void getWelcomePage(){
+        Intent start = new Intent(getApplicationContext(), WelcomePage.class);
+        startActivity(start);
+    }
+    //start cooking activity
+    private void getCookingPage(){
+        Intent start = new Intent(getApplicationContext(), CookingBasicsActivity.class);
+        startActivity(start);
+    }
+    //start budget activity
+    private void getBudgetPage(){
+        Intent start = new Intent(getApplicationContext(), HowToMakeABudgetActivity.class);
+        startActivity(start);
+    }
+    //start depression activity
+    private void getDepressionPage(){
+        Intent start = new Intent(getApplicationContext(), WorkingThroughDepressionActivity.class);
+        startActivity(start);
+    }
+    //start signIn activity
+    private void getSignInPage(){
+        Intent start = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(start);
+    }
+    //create menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //switch to fire menu options
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.signUpbtn:
+                getSignUpActivity();
+                return true;
+            case R.id.signInbtn:
+                getSignInPage();
+                return true;
+            case R.id.profilebtn:
+                getProfile();
+                return true;
+            case R.id.mainContentbtn:
+                getMainContent();
+                return true;
+            case R.id.welcomepgbtn:
+                getWelcomePage();
+                return true;
+            case R.id.budgetbtn:
+                getBudgetPage();
+                return true;
+            case R.id.cookingbtn:
+                getCookingPage();
+                return true;
+            case R.id.depressionbtn:
+                getDepressionPage();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
